08ff5c30b578b8f7f3f7ff07e69b495245709d5d
Nicole Gu <nicoleguob@gmail.com>
2021-08-31 23:37:03 -0700

    Initialized DVC

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash --login
 
 mkdir -p /workdir/media/ /workdir/incorrect_images/ /workdir/saved_models/
-dvc get # https://github.com/iterative/dataset-registry tutorials/versioning/data.zip
+dvc get https://github.com/nclgbd/ASL-Alphabet.git data.zip
 unzip data.zip
