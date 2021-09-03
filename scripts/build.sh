e3e62758f03f6c3658b6291f9d88687accd2c86b
Nicole Gu <nicoleguob@gmail.com>
2021-09-03 01:46:46 -0700

    Switching branches

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash --login
 
 mkdir -p /workdir/media/ /workdir/incorrect_images/ /workdir/saved_models/
+
+# get zip file from google doc
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QZj_E8ixxh4n2Bv4AcSCcFayNejeBQV0' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1QZj_E8ixxh4n2Bv4AcSCcFayNejeBQV0" -O data.zip && rm -rf /tmp/cookies.txt
-unzip data.zip
+
+unzip -q data.zip
