0df81d805f5d975275fec6b0745320d07c14ebcd
Nicole Gu <nicoleguob@gmail.com>
2021-09-06 21:58:31 -0700

    Reuploaded zip file, going to build docker image.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@
 mkdir -p /workdir/media/ /workdir/incorrect_images/ /workdir/saved_models/
 
 # get zip file from google doc
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QZj_E8ixxh4n2Bv4AcSCcFayNejeBQV0' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1QZj_E8ixxh4n2Bv4AcSCcFayNejeBQV0" -O ./data.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1CHWKCgTiasvlkp7JvP4nG_1KmaWa4U3v' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1CHWKCgTiasvlkp7JvP4nG_1KmaWa4U3v" -O ./data.zip && rm -rf /tmp/cookies.txt
 unzip -f ./data.zip
 rm -f ./data.zip
